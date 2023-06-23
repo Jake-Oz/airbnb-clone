d52072f4633719ff2cde1d34141ebe946ebfd550
Jake-Oz <jakeandrach@me.com>
2023-06-23 21:47:37 +1000

    Updated with Lint errors corrected

--- app/components/navbar/usermenu.tsx
+++ app/components/navbar/usermenu.tsx
@@ -32,7 +32,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     }
 
     return rentModal.onOpen();
-  }, [currentUser, loginModal]);
+  }, [currentUser, loginModal, rentModal]);
 
   return (
     <div className="relative">
